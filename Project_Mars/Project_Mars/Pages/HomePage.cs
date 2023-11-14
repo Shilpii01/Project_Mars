d40af26d33476dcc1e4c63ff8f8f885f59d4933a
Shilpii01 <shilpicharu@gmail.com>
2023-11-14 19:49:40 +1300

    Added POM

---
+++ Project_Mars/Project_Mars/Pages/HomePage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using static Project_Mars.Utilities.CommonDriver;
+
+
+namespace Project_Mars.Pages
+{
+    public class HomePage
+    {
+        
+        public void SelectLanguageTab()
+        {
+            Thread.Sleep(2000);
+
+            //Wait.waitToBeVisible(driver, "XPath", "//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[1]", 2);
+            IWebElement LanguageTab = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[1]"));
+            LanguageTab.Click();
+
+        }
+
+        public void SelectSkillTab()
+        {
+            Thread.Sleep(2000);
+            //Wait.waitToBeVisible(driver, "XPath", "//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[1]", 2);
+            IWebElement SkillTab = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[1]"));
+            SkillTab.Click();
+
+        }
+    }
+}
+
---
+++ Project_Mars/Project_Mars/Pages/LoginPage.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using static Project_Mars.Utilities.CommonDriver;
+
+
+namespace Project_Mars.Pages
+{
+    public class LoginPage
+    {
+
+        public void SignInActions()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:5000/Home");
+            Thread.Sleep(2000);
+            // Wait.waitToBeVisible(driver, "XPath", "//*[@id=\"home\"]/div/div/div[1]/div/a", 5);
+            IWebElement SignInButton = driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
+            SignInButton.Click();
+
+        }
+        public void LogInActions()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:5000/");
+
+            try
+            {
+                //click Sign In
+                IWebElement signInButton = driver.FindElement(By.LinkText("Sign In"));
+                signInButton.Click();
+
+                //Enter email address
+                IWebElement emailAddress = driver.FindElement(By.XPath("//input[@placeholder='Email address']"));
+                emailAddress.SendKeys("shilpicharu@gmail.com");
+
+                //Enter password
+                IWebElement password = driver.FindElement(By.Name("password"));
+                password.SendKeys("qwerty");
+
+                //Click Login
+                IWebElement loginButton = driver.FindElement(By.XPath("//button[contains(text(),'Login')]"));
+                loginButton.Click();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Portal does not launch.", ex.Message);
+            }
+
+        }
+    }
+}
